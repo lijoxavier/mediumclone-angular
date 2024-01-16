e18599fc1a6a43b7065323ad96bac43b3c46bfae
lijoxavier <lijoxavier303@gmail.com>
2024-01-16 18:03:59 +0530

    feat: Creating feed module

---
+++ src/app/shared/components/feed/feed.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"mc-feed",
+    templateUrl:"./feed.component.html",
+    standalone:true
+})
+
+export class FeedComponent {
+    
+}
