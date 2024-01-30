fb2ecd5ee5d2449ea5d4f558bccb4a7cf41ff47c
lijoxavier <lijoxavier303@gmail.com>
2024-01-30 12:54:15 +0530

    feat: Creating article form

--- src/app/shared/components/articleForm/articleForm.component.ts
+++ src/app/shared/components/articleForm/articleForm.component.ts
@@ -1,17 +1,54 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { ArticleFormValuesInterface } from "./types/articleFormValues.interface";
 import { BackendErrorsInterface } from "../../types/backendErrors.interface";
+import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { backendErrorMessages } from "../backendErrorMessages/backendErrorMessages.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
-    selector:"mc-article-form",
-    templateUrl:"./articleForm.component.html",
-    standalone:true
+  selector: 'mc-article-form',
+  templateUrl: './articleForm.component.html',
+  standalone: true,
+  imports:[backendErrorMessages,CommonModule,ReactiveFormsModule]
 })
+export class ArticleFormComponent implements OnInit {
+  @Input() initialValues?: ArticleFormValuesInterface
+  @Input() isSubmitting: boolean = false
+  @Input() errors: BackendErrorsInterface | null = null
 
-export class ArticleFormComponent {
-    @Input() initialValues?:ArticleFormValuesInterface
-    @Input() isSubmitting:boolean = false
-    @Input() errors:BackendErrorsInterface | null = null
+  @Output() articleSubmit = new EventEmitter<ArticleFormValuesInterface>()
 
-    @Output() articleSubmit = new EventEmitter<ArticleFormValuesInterface>()
+  form = this.fb.nonNullable.group({
+    title: '',
+    description: '',
+    body: '',
+    tagList: ''
+  })
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm(): void {
+    if (!this.initialValues) {
+      throw new Error('Inputs are not provided')
+    }
+    this.form.patchValue({
+      title: this.initialValues.title,
+      description:this.initialValues.description,
+      body:this.initialValues.body,
+      tagList:this.initialValues.tagList.join(' ')
+    })
+  }
+
+  onSubmit():void {
+    const formValue = this.form.getRawValue()
+    const articleFormValues:ArticleFormValuesInterface = {
+        ...formValue,
+        tagList:formValue.tagList.split(' ')
+    }
+    this.articleSubmit.emit(articleFormValues)
+  }
 }
