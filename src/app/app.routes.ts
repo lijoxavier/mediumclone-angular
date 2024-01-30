8e950271b4eb981b5ca01c0476095c1c4ee14379
lijoxavier <lijoxavier303@gmail.com>
2024-01-30 08:51:44 +0530

    feat: Create article

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,7 +25,12 @@ export const appRoutes: Route[] = [
     loadChildren:()=>import("src/app/tagFeed/tagFeed.routes").then((m)=>m.routes)
   },
   {
+    path:"articles/new",
+    loadChildren:()=>import("src/app/createArticle/createArticle.routes").then((m)=>m.routes)
+  },
+  {
     path:"articles/:slug",
     loadChildren:()=>import("src/app/article/article.route").then((m)=>m.routes)
-  }
+  },
+  
 ]
